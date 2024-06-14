f0651f9d90ccf4f06f0f538b0c1c26bc1602f7f6
Fahim047 <fahimulislam58@gmail.com>
2024-06-14 15:45:13 +0600

    🐛 Fixed the search option
    fixed a bug in the search box

--- src/data/location-data.js
+++ src/data/location-data.js
@@ -1,53 +1,78 @@
 const data = [
 	{
-		name: 'Central Park',
-		latitude: 40.785091,
-		longitude: -73.968285,
+		name: 'New York',
+		latitude: 40.712776,
+		longitude: -74.005974,
 	},
 	{
-		name: 'Eiffel Tower',
-		latitude: 48.858844,
-		longitude: 2.294351,
+		name: 'London',
+		latitude: 51.507351,
+		longitude: -0.127758,
 	},
 	{
-		name: 'Great Wall of China',
-		latitude: 40.431908,
-		longitude: 116.570374,
+		name: 'Tokyo',
+		latitude: 35.689487,
+		longitude: 139.691711,
 	},
 	{
-		name: 'Sydney Opera House',
-		latitude: -33.856784,
-		longitude: 151.215297,
+		name: 'Paris',
+		latitude: 48.856613,
+		longitude: 2.352222,
 	},
 	{
-		name: 'Christ the Redeemer',
-		latitude: -22.951916,
-		longitude: -43.210487,
+		name: 'Sydney',
+		latitude: -33.86882,
+		longitude: 151.209296,
 	},
 	{
-		name: 'Mount Everest',
-		latitude: 27.988121,
-		longitude: 86.925026,
+		name: 'Berlin',
+		latitude: 52.520008,
+		longitude: 13.404954,
 	},
 	{
-		name: 'Pyramids of Giza',
-		latitude: 29.979235,
-		longitude: 31.134202,
+		name: 'Moscow',
+		latitude: 55.755825,
+		longitude: 37.617298,
 	},
 	{
-		name: 'Taj Mahal',
-		latitude: 27.175015,
-		longitude: 78.042155,
+		name: 'Dubai',
+		latitude: 25.204849,
+		longitude: 55.270782,
 	},
 	{
-		name: 'Statue of Liberty',
-		latitude: 40.689247,
-		longitude: -74.044502,
+		name: 'Singapore',
+		latitude: 1.352083,
+		longitude: 103.819839,
 	},
 	{
-		name: 'Machu Picchu',
-		latitude: -13.163141,
-		longitude: -72.544963,
+		name: 'Rio de Janeiro',
+		latitude: -22.906847,
+		longitude: -43.172897,
+	},
+	{
+		name: 'Dhaka',
+		latitude: 23.810331,
+		longitude: 90.412521,
+	},
+	{
+		name: 'Kolkata',
+		latitude: 22.572645,
+		longitude: 88.363892,
+	},
+	{
+		name: 'Mumbai',
+		latitude: 19.07609,
+		longitude: 72.877426,
+	},
+	{
+		name: 'Beijing',
+		latitude: 39.904202,
+		longitude: 116.407394,
+	},
+	{
+		name: 'Los Angeles',
+		latitude: 34.052235,
+		longitude: -118.243683,
 	},
 ];
 
@@ -60,7 +85,7 @@ function getLocationByName(location) {
 	if (!location) return null;
 
 	const filteredLocation = data.filter(
-		(item) => item.name === location.toLowerCase()
+		(item) => item.name.toLowerCase() === location.toLowerCase()
 	);
 	// console.log(filteredLocation);
 	if (filteredLocation.length > 0) return filteredLocation[0];
